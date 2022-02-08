6fdf6d8300acd7928ff317635c7facc8a386ba75
Jefte S.A <jsap@hotmail.com.br>
2022-02-07 22:56:00 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AWS Tutorial DevOps - Hello World!'));
+app.get('/', (req, res) => res.send('AWS Tutorial DevOps - Pipeline - Hello World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
